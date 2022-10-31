a59e2e416469d2885d0e592d9b963070785812ff
Bodyshots <masterofbodyshots@gmail.com>
2022-10-31 01:01:39 -0400

    [MAIN] added cycle equality func + setup for cycle sort comparison to other sorts

--- cycle_multiply.py
+++ cycle_multiply.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 FIRST_ELEM = 0
 
@@ -9,9 +9,9 @@ def simplify(expr: List[List[int]]) -> List[List[int]]:
     
     >>> simplify([])
     []
-    >>> simplify([[1,3,2], [1,3,2]])
+    >>> simplify([[1, 3, 2], [1, 3, 2]])
     [[1, 2, 3]]
-    >>> simplify([[1,5,4], [3], [2,6], [1,5,4], [3], [2,6]])
+    >>> simplify([[1, 5, 4], [3], [2, 6], [1, 5, 4], [3], [2, 6]])
     [[1, 4, 5], [3], [2], [6]]
     
     """
@@ -32,16 +32,53 @@ def simplify(expr: List[List[int]]) -> List[List[int]]:
     while curr_elem != None:
         elem_insert = end_cycle_num(expr, curr_elem)
         if elem_insert in curr_cycle:
-            if curr_cycle: simplified_expr.append(curr_cycle)
+            if curr_cycle: 
+                simplified_expr.append(curr_cycle)
+            
             curr_cycle = []
-            if all_nums: curr_elem = all_nums[FIRST_ELEM]
-            else: curr_elem = None
+            
+            if all_nums: 
+                curr_elem = all_nums[FIRST_ELEM]
+            else: 
+                curr_elem = None
         else:
             curr_cycle.append(elem_insert)
             all_nums.remove(elem_insert)
             curr_elem = elem_insert
 
     return simplified_expr
+
+def equal_perm(expr1: List[List[int]], expr2: List[List[int]]) -> bool:
+    """
+    Return True if <expr1>'s elements have the same mapping as <expr2>.
+    Return False otherwise.
+
+    >>> equal_perm([[1, 2, 3]], [[1, 2, 3]])
+    True
+    >>> equal_perm([[2, 1, 3]], [[1, 2, 3]])
+    False
+    >>> equal_perm([[2, 1, 3]], [[1, 2, 3]])
+    False
+    >>> equal_perm([[1, 3, 2], [1, 3, 2]], [[1, 2, 3]])
+    False
+    >>> equal_perm([[1, 5, 4], [3], [2, 6], [1, 5, 4], [3], [2, 6]],\
+                   [[1, 4, 5], [3], [2], [6]])
+    False
+
+    """
+    get_mapping(expr1) == get_mapping(expr2) # Note: order doesn't matter when comparing dicts
+
+def get_mapping(expr: List[List[int]]) -> Dict[int, int]:
+    true_expr = simplify(remove_singletons(expr))
+    mapping = {}
+
+    for cycle in true_expr:
+        for index, elem in enumerate(cycle):
+            if index == len(cycle) - 1:
+                mapping[elem] = cycle[FIRST_ELEM]
+            else: 
+                mapping[elem] = cycle[index + 1]
+    return mapping
 
 def end_cycle_num(expr: List[List[int]], num: int) -> int:
     for cycle in range(len(expr) -1, -1, -1):
@@ -54,12 +91,17 @@ def end_cycle_num(expr: List[List[int]], num: int) -> int:
 
 def remove_dups(lst: List[int]) -> List[int]:
     all_nums = set()
-    set_add = all_nums.add # seen_add = seen.add, since its faster to use methods via local vars
 
     # basically, add an element to our new list if:
     # - the element is not in our set and...
     # - adding to the set is successful (since set.add() rets None)
-    return [elem for elem in lst if not (elem in all_nums or set_add(elem))]
+    return [elem for elem in lst if not (elem in all_nums or all_nums.add(elem))]
 
 def remove_singletons(lst: List[List[int]]) -> List[List[int]]:
     return [elem for elem in lst if len(elem) > 1]
+
+if __name__ == "__main__":
+    equal_perm([[2, 1, 3]], [[1, 2, 3]])
+    
+    # import doctest
+    # doctest.testmod()
---
+++ cycle_sort.py
@@ -0,0 +1,41 @@
+from typing import List
+"""
+According to Wikipedia, cycle sort is meant to minimize the number of writes required to sort
+an array. So, in light of its terrible worst-case, best-case, and average-case performance
+(O(n^2)), this module is confirm the advantages cycle sort has over other in-place sorting
+algorithms.
+
+"""
+
+def cycle_sort(arr: List[int]) -> List[int]:
+    """
+    Return the elements of <arr> in ascending order using cycle permutations.
+    Also, print the:
+        - Cycle permutations created while sorting
+        - The total number of writes performed 
+    
+    """
+    pass
+
+def bubble_sort(arr: List[int]) -> List[int]:
+    """
+    Return the elements of <arr> in ascending order using bubble sort.
+    Also, print the total number of writes performed
+    
+    Bubble sort is typically known as the "worst" sorting in-place
+    sorting algorithm (O(n^2) for everything).
+
+    """
+    pass
+
+def quick_sort(arr: List[int]) -> List[int]:
+    """
+    Return the elements of <arr> in ascending order using quick sort.
+    Also, print the total number of writes performed
+    
+    Quick sort is typically regarded as one of the best in-place
+    sorting algorithms, especially in its average case running time (O(nlogn))
+
+    """
+    pass
+
